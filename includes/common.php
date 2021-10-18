4765266fd6d12f28470d10f385c9669b9a9f160d
Shalini Pathak <86199053+letslearnn@users.noreply.github.com>
2021-10-18 10:49:12 +0530

    Create common.php

---
+++ includes/common.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = '12345';
+$db_name = "LifestyleStore";
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $db_name);
+// Check connection
+if (!$con) {
+ die("Connection failed: " . mysqli_connect_error());
+}
+if(session_id() == '')
+session_start();
+?>
