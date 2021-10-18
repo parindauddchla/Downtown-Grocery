bb6206a96091bafad26a486d630452c7d6081bdf
Shalini Pathak <86199053+letslearnn@users.noreply.github.com>
2021-10-18 10:52:08 +0530

    Update common.php

--- includes/common.php
+++ includes/common.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = '12345';
-$db_name = "LifestyleStore";
+$db_name = "Downtown Grocery";
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $db_name);
 // Check connection
