99bbd4437f78a19c671d165f69cd2a10256b96cc
tushar-harsh <92385119+tushar-harsh@users.noreply.github.com>
2021-11-15 12:52:42 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,5 @@
-<footer>
-    <div class="container">
-        <center>
-            <p>Copyright &copy; Downtown Grocery. All Rights Reserved  |  Contact Us: +91 87654 32102</p>	
-        </center>
-    </div>
+<footer class="bg-success text-light">
+    <center>
+        <p>Copyright &copy; Downtown Grocery</p>
+    </center>
 </footer>
