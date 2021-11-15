b8580056c6c441af7c18a81339d24638e1e182a4
tushar-harsh <92385119+tushar-harsh@users.noreply.github.com>
2021-11-15 12:54:15 +0530

    required changes for the file as implemented

--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,29 @@
-<div class="navbar navbar-inverse navbar-fixed-top">
+<div class="navbar navbar-expand-lg navbar-dark bg-success">
     <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>                        
-            </button>
-            <a class="navbar-brand" href="index.php">Downtown Grocery</a>
-        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
         <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav navbar-right">
-              
+            <ul class="navbar-nav">
+                <?php
+                if (isset($_SESSION['email'])) {
+                ?>
+                    <li style="margin-right:20px;"><a class="text-light" href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart </a></li>
+                    <li style="margin-right:20px;"><a class="text-light" href="settings.php"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
+                    <li><a class="text-light" href="logout_script.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+                <?php
+                } else {
+                ?>
+                    <li style="margin-right:20px;"><a class="text-light" href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a class="text-light" href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                <?php
+                }
+                ?>
             </ul>
         </div>
+
+        <a class=" navbar-brand" href="index.php">Downtown Grocery</a>
     </div>
+
 </div>
