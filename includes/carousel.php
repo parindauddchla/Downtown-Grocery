1ba170e4f60b6fce77e3f0e31b98275a5da337ac
tushar-harsh <92385119+tushar-harsh@users.noreply.github.com>
2021-11-19 15:04:14 +0530

    Create carousel.php
    Carousel used for providing basis for informative content

---
+++ includes/carousel.php
@@ -0,0 +1,21 @@
+<div id="custom_carousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner" style="height:40rem;">
+        <div class="carousel-item active">
+            <span class="d-block custom-slide1"></span>
+        </div>
+        <div class="carousel-item">
+            <span class="d-block custom-slide2"></span>
+        </div>
+        <div class="carousel-item">
+            <span class="d-block custom-slide3"></span>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#custom_carousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#custom_carousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
